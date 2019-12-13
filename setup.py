6465aeddd71ab0bafe65a3962e8ce7c5b9714398
Fedor Garin <fedor@pymetrics.com>
2019-12-12 23:43:53 -0500

    go to 0.2

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ README = open(os.path.join(here, 'README.md')).read()
 
 setup(
     name='django-data-sniffer',
-    version='0.1.1',
+    version='0.2.0',
     packages=[
         'data_sniffer',
     ],
